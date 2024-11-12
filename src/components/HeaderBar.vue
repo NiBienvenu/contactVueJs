dde8d06d083583adea6ba17b1c1dd2df2d2f7727
NiBienvenu <nicibienvenu@gmail.com>
2024-11-12 17:23:19 +0200

    component

---
+++ src/components/HeaderBar.vue
@@ -0,0 +1,23 @@
+<template lang="">
+  <nav>
+    <h1>HeaderBar</h1>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link> |
+    <router-link to="/contact">Contact</router-link>
+
+  </nav>
+</template>
+
+<style>
+
+  nav {
+    width: 100%;
+    height: 50px;
+    box-sizing: border-box;
+    top: 0;
+   
+  }
+
+
+  
+</style>
